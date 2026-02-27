bb8fe8264a67494cf48e16b92a52ccc8744a9edd
elias <jcruz895@unab.edu.co>
2026-02-26 22:14:29 -0500

    Jecpacl compose

--- app/src/main/java/me/eliascruz/jecpacktaller/MainActivity.kt
+++ app/src/main/java/me/eliascruz/jecpacktaller/MainActivity.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -105,4 +106,43 @@ fun PcproductoScreen() {
 
 
 
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun ArticleScreen() {
 
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.jecpack),
+            contentDescription = "Portada",
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(180.dp)
+        )
+
+        Text(
+            text = "Jetpack Compose tutorial",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(16.dp)
+        )
+
+        Text(
+            text = "Jetpack Compose is a modern toolkit for building native Android UI...",
+            modifier = Modifier.padding(horizontal = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text(
+            text = "In this tutorial, you build a simple UI component...",
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
+}
